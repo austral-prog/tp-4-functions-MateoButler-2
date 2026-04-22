0913841f58edeb6dec9a1ab2b4e0cc331ebf4799
MateoButler <mateobutler06@gmail.com>
2026-04-21 23:41:07 -0300

    Add files via upload

--- quadratic.py
+++ quadratic.py
@@ -20,9 +20,20 @@ def value_y(a, b, c, x):
     return a * x**2 + b * x + c
 
 def to_string(a, b, c):
+    # Caso result3: Si a y b son 0, solo queda el número solo
+    if a == 0 and b == 0:
+        return f"f(x) = {c}"
 
-    return (f"f(x) = {a} * X^2  + {b} * X + {c}")
+    # Caso result2: Si a es 0, empieza en el término X
+    if a == 0:
+        return f"f(x) = {b} * X + {c}"
 
+    # Caso result4: Si b es 0, aquí es donde van los DOS ESPACIOS
+    if b == 0:
+        return f"f(x) = {a} * X^2  + {c}"
+
+    # Caso result1: Si están todos los números, va con UN SOLO ESPACIO
+    return f"f(x) = {a} * X^2 + {b} * X + {c}"
 
 def derivation(a, b, c):
     der_a = 2 * a
