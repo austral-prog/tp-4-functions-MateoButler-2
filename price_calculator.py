8c6ed7581c8700103761def9fbcfb89ae5046818
MateoButler <mateobutler06@gmail.com>
2026-04-21 23:08:42 -0300

    Add files via upload

--- price_calculator.py
+++ price_calculator.py
@@ -21,7 +21,7 @@ def final_price(price, quantity, discount_pct, tax_pct):
       3. Aplicar el impuesto al resultado usando apply_tax.
       4. Retornar el resultado redondeado a 2 decimales usando round().
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    return round(apply_tax(apply_discount((price * quantity), discount_pct), tax_pct), 2)
 
 def best_deal(price_a, qty_a, disc_a, price_b, qty_b, disc_b, tax_pct):
     """
@@ -31,4 +31,8 @@ def best_deal(price_a, qty_a, disc_a, price_b, qty_b, disc_b, tax_pct):
     Si son iguales, retorna "A".
     Debe USAR la función final_price para resolver el ejercicio.
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    if final_price(price_a, qty_a, disc_a, tax_pct) >= final_price(price_b, disc_b, qty_b, tax_pct):
+        return "A"
+    else:
+        return "B"
+
--- quadratic.py
+++ quadratic.py
@@ -1,16 +1,30 @@
 # Replace the "ANSWER HERE" for your answer
 
 def roots(a, b, c):
-    return "ANSWER HERE"
+
+    if (b**2 - 4 * a * c) >= 0:
+
+        r1 = (-1 * b + (b**2 - 4 * a * c )**0.5) / (2 * a)
+        r2 = (-1 * b - (b**2 - 4 * a * c )**0.5) / (2 * a)
+
+        if r1 != r2:
+            return (f"({r1}, {r2})")
+        else:
+            return (f"({r1})")
+    else:
+        return ("( )")
 
 
 def value_y(a, b, c, x):
-    return "ANSWER HERE"
+
+    return a * x**2 + b * x + c
 
 
 def to_string(a, b, c):
-    return "ANSWER HERE"
+
+    return (f"f(x) = {a} * X^2 + {b} * X + {c}")
 
 
 def derivation(a, b, c):
-    return "ANSWER HERE"
+
+    return (f"f'(x) = {2 * a}x + {b}")
